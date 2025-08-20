a76ea7da248bb32dfac9aa7f0aad1d3eb07ea2eb
Cornelius Appia-Kubi <appiakubi4u@yahoo.com>
2025-08-20 10:34:08 +0000

    Create cppgoals1.cpp

---
+++ cppgoals1.cpp
@@ -0,0 +1,78 @@
+Welcme to Cornelius-Snowhyt's C++ journey!
+
+This contains my ambitions and learning path in c++:
+1.Master the basics:
+-Variables
+-Loops
+-Functions
+2.Improve my African Currency Converter projects.
+3.Build a calculator program.
+4.Learn Object-Orientwd Programming(OOP).
+5.Later, explore data structures and algoriyhms.
+
+
+
+
+
+/==================================================
+//====CORNELIUS-SNOWHYT-C++ Programming Ambitions===
+//==================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {  
+    cout << "==================================================\n";
+    cout << "CORNELIUS-SNOWHYT C++ Progamming Ambitions========\n";
+    cout << "=================================================\n\n";
+    
+    cout << "Welcome to CORNELIUS-SNOWHYT's C++ journey!\n\n";
+    cout << "*/\n"
+         << "This contains my ambitions and learning path in C++.\n\n"
+         << "1. Master the basics: \n"
+         <<  "  --Variables\n"
+         <<  "  --Loops \n" 
+         <<  "  --Functions\n"
+         << "2. Improve my African Currency Converter project.\n"
+         << "3. Build a calculator program\n"      
+         << "4. Learn Object-Oriented Programming (OOP)\n"
+         << "5. Later, explore data structures and algorithms.\n" 
+         << "6. Contribute to open-source C++ projects.\n"
+         << "7. Ultimately become a Data Scientist.\n" 
+         << "*/\n\n" 
+
+         << "======Stay consistent and keep building ======= \n\n";
+    cout << "=======DEO CONFIDIMIUS==========\n";     
+
+    return 0;       
+
+}
+
+
+
+
+==================================================
+CORNELIUS-SNOWHYT C++ Progamming Ambitions========
+=================================================
+
+Welcome to CORNELIUS-SNOWHYT's C++ journey!
+
+*/
+This contains my ambitions and learning path in C++.
+
+1. Master the basics: 
+  --Variables
+  --Loops 
+  --Functions
+2. Improve my African Currency Converter project.
+3. Build a calculator program
+4. Learn Object-Oriented Programming (OOP)
+5. Later, explore data structures and algorithms.
+6. Contribute to open-source C++ projects.
+7. Ultimately become a Data Scientist.
+*/
+
+======Stay consistent and keep building ======= 
+
+=======DEO CONFIDIMIUS==========
+snow@snow-Lenovo-IdeaPad-U410-Touch:~/Documents$ 

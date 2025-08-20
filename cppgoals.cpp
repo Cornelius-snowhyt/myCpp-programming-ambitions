9389005e80462d30e20dc4502eec487789be9cc7
Cornelius Appia-Kubi <appiakubi4u@yahoo.com>
2025-08-20 10:11:47 +0000

    Add files via upload

---
+++ cppgoals.cpp
@@ -0,0 +1,33 @@
+//==================================================
+//====CORNELIUS-SNOWHYT-C++ Programming Ambitions===
+//==================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {  
+    cout << "==================================================\n";
+    cout << "CORNELIUS-SNOWHYT C++ Progamming Ambitions========\n";
+    cout << "=================================================\n\n";
+    
+    cout << "Welcome to CORNELIUS-SNOWHYT's C++ journey!\n\n";
+    cout << "*/\n"
+         << "This contains my ambitions and learning path in C++.\n\n"
+         << "1. Master the basics: \n"
+         <<  "  --Variables\n"
+         <<  "  --Loops \n" 
+         <<  "  --Functions\n"
+         << "2. Improve my African Currency Converter project.\n"
+         << "3. Build a calculator program\n"      
+         << "4. Learn Object-Oriented Programming (OOP)\n"
+         << "5. Later, explore data structures and algorithms.\n" 
+         << "6. Contribute to open-source C++ projects.\n"
+         << "7. Ultimately become a Data Scientist.\n" 
+         << "*/\n\n" 
+
+         << "======Stay consistent and keep building ======= \n\n";
+    cout << "=======DEO CONFIDIMIUS==========\n";     
+
+    return 0;       
+
+}
